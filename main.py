370d1d0beab09f8538de1bc6b7ac85c9400fa5c9
txks04a-dot <txks04a@gmail.com>
2026-02-27 10:50:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-```python
 import os
 import json
 import uuid
@@ -952,3 +951,4 @@ def metrics():
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", "10000"))
   app.run(host="0.0.0.0", port=port)
+
